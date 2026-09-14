35ac756eaf61c297f82c141c51aa83dfd1c14bcc
03-25-03-smt <vlad40303@gmail.com>
2026-09-14 15:09:55 +0200

    Update get_next_line.h

--- get_next_line.h
+++ get_next_line.h
@@ -1,21 +1,18 @@
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
 
-# include <stdio.h>
 # include <unistd.h>
 # include <stdlib.h>
-# include <sys/types.h>
-# include <sys/stat.h>
-# include <fcntl.h>
+# include <sys/types.h>                                                   // Системные типы, в частности ssize_t -> это знаковый целочисленный тип, то есть он может хранить и положительные, и отрицательные значения.
 
 # ifndef BUFFER_SIZE
-# define BUFFER_SIZE 42
+#  define BUFFER_SIZE 42
 # endif
 
 char	*get_next_line(int fd);
 
-size_t strlen_at(const char *s, int end);
-char *find_chr(const char *s, int c);
+size_t	strlen_at(const char *s, int end);
+char	*find_chr(const char *s, int c);
 void	*ft_memcpy(void *dest, const void *src, size_t n);
 char	*cpy_buffer(const char *buffer, size_t start, size_t len);
 char	*merge_previous_and_current(char *previous, const char *current);
