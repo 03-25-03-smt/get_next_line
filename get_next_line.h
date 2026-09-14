1a039299c2e061b9cbbddccc406945bc321d3560
03-25-03-smt <vlad40303@gmail.com>
2026-09-14 13:05:09 +0200

    Update get_next_line.h

--- get_next_line.h
+++ get_next_line.h
@@ -9,5 +9,10 @@
 # endif
 
 char	*get_next_line(int fd);
+size_t strlen_at(const char *s, int end);
+char *find_chr(const char *s, int c);
+void	*ft_memcpy(void *dest, const void *src, size_t n);
+char	*cpy_buffer(const char *buffer, size_t start, size_t len);
+char	*merge_previous_and_current(char *previous, const char *current);
 
 #endif
